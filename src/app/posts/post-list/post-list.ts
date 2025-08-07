33dc6a3e749f9549594ce9915fbe0cf86086b462
Sofia Ayala <sofiarayala1995@gmail.com>
2025-08-07 11:34:45 +0200

    added signals instead of event emmitters

--- src/app/posts/post-list/post-list.ts
+++ src/app/posts/post-list/post-list.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, effect } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { PostInterface } from '../interfaces/post-interface';
+import { PostsService } from '../../services/posts-service';
 
 @Component({
   selector: 'app-post-list',
@@ -8,6 +9,12 @@ import { PostInterface } from '../interfaces/post-interface';
   templateUrl: './post-list.html',
   styleUrl: './post-list.css',
 })
-export class PostList {
-  @Input() posts: PostInterface[] = [];
+export class PostList implements OnInit {
+  posts: any;
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.postsSignal;
+  }
 }
--- src/app/services/posts-service.ts
+++ src/app/services/posts-service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { PostInterface } from '../posts/interfaces/post-interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
-  
+  private posts: PostInterface[] = [];
+  private postsUpdated = signal<PostInterface[]>([]);
+
+  getPosts() {
+    return [...this.posts];
+  }
+
+  get postsSignal() {
+    return this.postsUpdated.asReadonly();
+  }
+
+  addPost(title: string, content: string) {
+    const post: PostInterface = { title: title, content: content };
+    this.postsUpdated.update((currentPosts) => [...currentPosts, post]);
+  }
 }
