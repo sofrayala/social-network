9d1e7534dc8d3eba638742fd8a81cc1cd794b901
Sofia Ayala <sofiarayala1995@gmail.com>
2025-08-21 11:56:39 +0200

    added POST backend point

--- back-end/app.js
+++ back-end/app.js
@@ -1,32 +1,47 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use((req, res, next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader
-    ("Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-  next();
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/posts',(req, res, next) => {
-  const posts =[
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/posts", (res, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully",
+  });
+});
+app.get("/api/posts", (req, res, next) => {
+  const posts = [
     {
-      id:'fash1234df', 
-      title:'First server-side post',
-      content:'This is coming from the server'
+      id: "fash1234df",
+      title: "First server-side post",
+      content: "This is coming from the server",
     },
     {
-      id:'kja1634df', 
-      title:'Second server-side post',
-      content:'This is coming from the server!'
+      id: "kja1634df",
+      title: "Second server-side post",
+      content: "This is coming from the server!",
     },
-  ];  
+  ];
   res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+    message: "Posts fetched successfully!",
+    posts: posts,
   });
 });
 
