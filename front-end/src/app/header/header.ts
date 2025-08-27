0bf7e11c8818d2644e9f284fdc8d9f9e2a8bd6c8
Sofia Ayala <sofiarayala1995@gmail.com>
2025-08-27 12:17:35 +0200

    feat: header: styling links

--- front-end/src/app/header/header.ts
+++ front-end/src/app/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, RouterLink],
+  imports: [MatToolbarModule, RouterLink, MatButton, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
