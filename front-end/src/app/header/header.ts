93a1ffbbb26d3d25dece0c4059590e06785b0d7d
Sofia Ayala <sofiarayala1995@gmail.com>
2025-08-27 11:38:44 +0200

    feat: header: added routing

--- front-end/src/app/header/header.ts
+++ front-end/src/app/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
