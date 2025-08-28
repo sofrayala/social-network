08f6b35ea256a8222fceeb414ec61183fe19fe36
Sofia Ayala <sofiarayala1995@gmail.com>
2025-08-28 15:29:35 +0200

    backend: reorganizinh routing routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
-import Post from "./models/post.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import postsRoutes from "./routes/posts.js";
 
 dotenv.config();
 const app = express();
@@ -34,60 +34,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "Post added successfully",
-      post: {
-        id: createdPost._id,
-        title: createdPost.title,
-        content: createdPost.content,
-      },
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "update successfull!" });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    console.log(documents);
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents,
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(400).json({ message: "Post not found" });
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted" });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 export default app;
