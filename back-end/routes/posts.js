79aa7559c1d6cbc64d4092cbee80fc38e2f1fdfd
Sofia Ayala <sofiarayala1995@gmail.com>
2025-09-01 15:29:46 +0200

    backend: posts.js: added multer & server side upload

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -1,9 +1,31 @@
 import express from "express";
 import Post from "../models/post.js";
+import multer from "multer";
 
 const router = express.Router();
 
-router.post("", (req, res, next) => {
+const MYME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mimetype");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "./back-end/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split("").join("-");
+    const ext = MYME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+router.post("",multer(storage).single("image") ,(req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
