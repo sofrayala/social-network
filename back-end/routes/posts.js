ec8be822a644ae49d400b5191a743b8b7f1cad23
Sofia Ayala <sofiarayala1995@gmail.com>
2025-09-04 11:26:38 +0200

    Pagination on back-end works

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -84,7 +84,15 @@ router.put(
 );
 
 router.get("", (req, res, next) => {
-  Post.find().then((documents) => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+
+  if (pageSize & currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+
+  postQuery.then((documents) => {
     console.log(documents);
     res.status(200).json({
       message: "Posts fetched successfully!",
